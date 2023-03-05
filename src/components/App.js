033e640fa705e3f1a102e8d6b3319ea9f657b0d2
Prashant Sonwane <prashants0209@gmail.com>
2023-03-05 18:56:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ListOfGoals = ({ goals }) => (
-  <ul>
-    {goals.map(({ goal, by }, i) => (
-      <li key={i}>
-        My goal is to {goal}, by {by}.
-      </li>
-    ))}
-  </ul>
-);
+const GoalForm = ({ addGoal }) => {
+  const [formData, setFormData] = useState({
+    goal: "",
+    by: ""
+  });
 
-export default ListOfGoals;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addGoal(formData);
+    setFormData({ goal: "", by: "" });
+  };
+
+  return (
+    <>
+      <h1>My Goals</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal..."
+          value={formData.goal}
+          onChange={(e) =>
+            setFormData({ ...formData, goal: e.target.value })
+          }
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By..."
+          value={formData.by}
+          onChange={(e) => setFormData({ ...formData, by: e.target.value })}
+        />
+        <button>Add</button>
+      </form>
+    </>
+  );
+};
+
+export default GoalForm;
